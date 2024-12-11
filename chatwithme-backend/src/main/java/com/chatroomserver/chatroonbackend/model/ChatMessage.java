1e5e50c5f93432167f29735c3cbc188978410a67
quocthai <lxlthailxl@gmail.com>
2024-12-11 16:00:42 +0700

    .gitignore is now working

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/model/ChatMessage.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/model/ChatMessage.java
@@ -20,5 +20,7 @@ public class ChatMessage {
     private String senderId;
     private String recipientId;
     private String content;
+    private String media;
+    private String mediaType;
     private Date timestamp;
 }
--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/message/MessageServiceImpl.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/message/MessageServiceImpl.java
@@ -1,4 +1,7 @@
 package com.chatroomserver.chatroonbackend.service.message;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MessageServiceImpl implements MessageService{
 }
