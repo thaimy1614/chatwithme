49e8fa65f946af43c6327df6faaab23567bed66b
quocthai <lxlthailxl@gmail.com>
2024-12-11 15:46:44 +0700

    add login resp dto

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/response/LoginResponse.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/response/LoginResponse.java
@@ -1,2 +1,11 @@
-package com.chatroomserver.chatroonbackend.dto.response;public class LoginResponse {
+package com.chatroomserver.chatroonbackend.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResponse {
+    private String token;
+    private String username;
 }
