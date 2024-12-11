65100d9dc840170248589d31166fb3ed6c04144f
quocthai <lxlthailxl@gmail.com>
2024-12-11 15:47:33 +0700

    login flow and handle generate jwt

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/user/UserService.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/user/UserService.java
@@ -1,4 +1,11 @@
 package com.chatroomserver.chatroonbackend.service.user;
 
+import com.chatroomserver.chatroonbackend.dto.request.LoginRequest;
+import com.chatroomserver.chatroonbackend.dto.response.LoginResponse;
+import com.nimbusds.jose.JOSEException;
+
 public interface UserService {
+    LoginResponse authenticate(LoginRequest loginRequest) throws JOSEException;
+
+    boolean introspect(String token);
 }
--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/user/UserServiceImpl.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/service/user/UserServiceImpl.java
@@ -6,25 +6,42 @@ import com.chatroomserver.chatroonbackend.exception.AppException;
 import com.chatroomserver.chatroonbackend.exception.ErrorCode;
 import com.chatroomserver.chatroonbackend.model.User;
 import com.chatroomserver.chatroonbackend.repository.UserRepository;
-import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, String> redisTemplate;
+    @Value("${jwt.signer-key}")
+    private String KEY;
+    @Value("${jwt.expiration-duration}")
+    private long EXPIRATION_DURATION;
+    @Value("${jwt.refreshable-duration}")
+    private String REFRESHABLE_DURATION;
 
-    public LoginResponse authenticate(LoginRequest request) {
+    public LoginResponse authenticate(LoginRequest request) throws JOSEException {
         String username = request.getUsername();
         String password = request.getPassword();
-        User authUser;
 
-            authUser = userRepository.findByUsername(username).orElseThrow(
-                    () -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        User authUser = userRepository.findByUsername(username).orElseThrow(
+                () -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
 
         boolean check = passwordEncoder.matches(password, authUser.getPassword());
@@ -35,7 +52,55 @@ public class UserServiceImpl implements UserService{
         var token = generateToken(authUser);
         return LoginResponse.builder()
                 .token(token)
-                .role(authUser.getRoles().getRole())
+                .username(username)
                 .build();
+    }
+
+    private String generateToken(User user) throws JOSEException {
+        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet
+                .Builder()
+                .subject(user.getUserId())
+                .issuer("Thaidq")
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(EXPIRATION_DURATION, ChronoUnit.SECONDS).toEpochMilli()))
+                .jwtID(UUID.randomUUID().toString())
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(jwsHeader, payload);
+        jwsObject.sign(new MACSigner(KEY));
+        return jwsObject.serialize();
+    }
+
+    private SignedJWT verifyToken(String token, boolean isRefresh) throws Exception {
+        JWSVerifier verifier = new MACVerifier(KEY.getBytes());
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        Date expiryTime = (isRefresh) ? new Date(signedJWT.getJWTClaimsSet().getIssueTime().toInstant().plus(Long.parseLong(REFRESHABLE_DURATION), ChronoUnit.SECONDS).toEpochMilli())
+                : signedJWT.getJWTClaimsSet().getExpirationTime();
+        var verified = signedJWT.verify(verifier);
+        if (!(verified && expiryTime.after(new Date()))) {
+            throw new Exception("UNAUTHENTICATED");
+        }
+
+        if (isTokenInBlacklist(signedJWT.getJWTClaimsSet().getJWTID())) {
+            throw new Exception("UNAUTHENTICATED");
+        }
+        return signedJWT;
+    }
+
+    public boolean introspect(String token) {
+        boolean isValid = true;
+        try {
+            verifyToken(token, false);
+        } catch (Exception e) {
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    private boolean isTokenInBlacklist(String jwtId) {
+        return redisTemplate.opsForValue().get("bl_" + jwtId) != null;
     }
 }
