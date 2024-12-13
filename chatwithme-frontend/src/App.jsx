6a44a6f7e26bda2ddc93702583a8f23f2b1d38ce
quocthai <lxlthailxl@gmail.com>
2024-12-13 10:24:26 +0700

    implement redirect url for oauth2

--- chatwithme-frontend/src/App.jsx
+++ chatwithme-frontend/src/App.jsx
@@ -1,37 +1,20 @@
-import {
-  Redirect,
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  useHistory,
-} from "react-router-dom";
-import axios from 'axios'
+import { Route, Routes, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { Login } from "./Layout/Login";
 import { ChatPage2 } from "./Layout/ChatPage2";
-axios.defaults.baseURL = import.meta.env.VITE_API_PREFIX || 'http://localhost:8080'
-axios.defaults.withCredentials = true
+import { Signup } from "./Layout/Signup";
+import Authenticate from "./component/common/Authenticate";
+axios.defaults.baseURL = import.meta.env.VITE_API_PREFIX || "http://localhost:8080";
+axios.defaults.withCredentials = true;
 function App() {
-  const history = useHistory();
-
-  if (localStorage.getItem("chat-username")) {
-    history.push("/chat");
-  } else history.push("/login");
-
   return (
-    <Router>
-      <Switch>
-        <Route exact path={"/"}>
-          <Redirect to={"/login"}></Redirect>
-        </Route>
-        <Route path={"/login"}>
-          <Login></Login>
-        </Route>
-
-        <Route path={"/chat"}>
-          <ChatPage2></ChatPage2>
-        </Route>
-      </Switch>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/chat" element={<ChatPage2 />} />
+      <Route path="/authenticate" element={<Authenticate />} />
+    </Routes>
   );
 }
 
---
+++ chatwithme-frontend/src/component/common/Authenticate.jsx
@@ -0,0 +1,75 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { setToken } from "../../services/localStorageService";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { fetchUserInfo } from "../../Layout/Login";
+
+export default function Authenticate() {
+  const navigate = useNavigate();
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  useEffect(() => {
+    console.log(window.location.href);
+
+    const authCodeRegex = /code=([^&]+)/;
+    const isMatch = window.location.href.match(authCodeRegex);
+
+    if (isMatch) {
+      const authCode = isMatch[1];
+
+      fetch(
+        `${import.meta.env.VITE_API_PREFIX}/user/outbound/authentication?code=${authCode}`,
+        {
+          method: "POST",
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+
+          setToken(data.result.token);
+          setIsLoggedin(true);
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchUserAndNavigate = async () => {
+      if (isLoggedin) {
+        try {
+          const userInfo = await fetchUserInfo(); // Wait for user info fetch
+
+          if (userInfo) {
+            navigate("/home"); // Navigate to home after fetching user info
+          } else {
+            console.error("Failed to retrieve user info.");
+          }
+        } catch (error) {
+          console.error("Error fetching user info:", error);
+        }
+      }
+    };
+
+    fetchUserAndNavigate();
+  }, [isLoggedin, navigate]);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress></CircularProgress>
+        <Typography>Đang xác minh...</Typography>
+      </Box>
+    </>
+  );
+}
