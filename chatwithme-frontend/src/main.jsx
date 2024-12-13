586c4eacba969b9d00fdf010f147ef603dbf9b65
quocthai <lxlthailxl@gmail.com>
2024-12-13 10:27:51 +0700

    update main, use ScrollToTop component

--- chatwithme-frontend/src/main.jsx
+++ chatwithme-frontend/src/main.jsx
@@ -1,11 +1,12 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./component/common/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
