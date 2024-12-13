92e4186d5413c7e4e6c4d73955ac2cd209906d2f
quocthai <lxlthailxl@gmail.com>
2024-12-13 10:28:09 +0700

    update login page

--- chatwithme-frontend/src/Layout/Login.jsx
+++ chatwithme-frontend/src/Layout/Login.jsx
@@ -1,44 +1,206 @@
-import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
-import "./button.css";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, TextField, Box, Container, Typography } from "@mui/material";
+import { OAuthConfig } from "../configurations/configuration";
+import {
+  getToken,
+  setToken,
+  setUserInfo,
+} from "../services/localStorageService";
+import Loading from "../component/common/loading";
+import MessageModal from "../component/common/message-modal";
+import axios from "axios";
+
+export const fetchUserInfo = async () => {
+  try {
+    const response = await axios.get("/user/my-info", {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    });
+    const data = await response.data;
+    setUserInfo(JSON.stringify(data.result));
+    return data.result;
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+  }
+};
 
 export const Login = () => {
-  const history = useHistory();
-  const [username, setUsername] = useState();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    localStorage.setItem("chat-username", username);
+  const handleClick = () => {
+    setModalOpen(false);
+    const callbackUrl = OAuthConfig.redirectUri;
+    const authUrl = OAuthConfig.authUri;
+    const googleClientId = OAuthConfig.clientId;
 
-    history.push("/chat");
+    const targetUrl = `${authUrl}?redirect_uri=${encodeURIComponent(
+      callbackUrl
+    )}&response_type=code&client_id=${googleClientId}&scope=openid%20email%20profile`;
+
+    console.log(targetUrl);
+
+    window.location.href = targetUrl;
   };
 
+  const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [messageType] = useState(false);
+  const [failMessage, setFailMessage] = useState(
+    "INCORRECT USERNAME OR PASSWORD!"
+  );
+
+  useEffect(() => {
+    const accessToken = getToken();
+
+    if (accessToken) {
+      navigate("/chat");
+    }
+  }, [navigate]);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setModalOpen(false);
+    setLoading(true);
+
+    const userData = { username, password };
+
+    try {
+      const response = await axios.post("/user/login", userData, {
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = response.data;
+      setLoading(false);
+
+      if (data.code === 1005) {
+        setFailMessage("Tài khoản không tồn tại!");
+        setModalOpen(true);
+        return;
+      }
+
+      if (data.code === 1008) {
+        setFailMessage("Tên đăng nhập hoặc mật khẩu không chính xác!");
+        setModalOpen(true);
+        return;
+      }
+
+      setToken(data.result.token);
+      const userInfo = await fetchUserInfo();
+
+      if (userInfo) {
+        navigate("/chat");
+      } else {
+        console.error("Failed to retrieve user info.");
+        setFailMessage("Hệ thống sập rồi!!!");
+        setModalOpen(true);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error("Login error:", error);
+      setFailMessage("Hệ thống sập rồi!!!");
+      setModalOpen(true);
+    }
+  };
+
+  const handleClose = () => {
+    setModalOpen(false);
+  };
   return (
-    <div
-      className=" d-flex align-items-center justify-content-center"
-      style={{
-        height: "100vh",
-        backgroundImage: `url("https://picsum.photos/1536/735?grayscale")`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="container form-group w-25  d-flex align-items-center justify-content-center gap-2">
-        <input
-          type="text"
-          name=""
-          id=""
-          className="rounded-3 form-control"
-          placeholder="Name"
-          onChange={(e) => setUsername(e.target.value)}
-          onKeyUp={(e) => {
-            console.log(e.key);
-            if (e.key == "Enter" || e.key == 13) handleLogin();
-          }}
+    <>
+      {loading && <Loading />}
+      <Box
+        sx={{
+          height: "100vh",
+          backgroundImage: `url("https://picsum.photos/1536/735?redscale")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Container maxWidth="xs">
+          <Box
+            sx={{
+              backgroundColor: "rgba(255, 255, 255, 0.9)",
+              padding: 4,
+              borderRadius: 2,
+              boxShadow: 3,
+            }}
+          >
+            <Typography variant="h5" align="center" gutterBottom>
+              Đăng nhập
+            </Typography>
+            <form noValidate>
+              <TextField
+                label="Tên đăng nhập"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <TextField
+                label="Mật khẩu"
+                type="password"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                onChange={(e) => setPassword(e.target.value)}
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") handleLogin();
+                }}
+                required
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                sx={{ marginTop: 2 }}
+                onClick={handleLogin}
+              >
+                Đăng nhập
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                fullWidth
+                sx={{ marginTop: 2 }}
+                className="google-signin"
+                onClick={handleClick}
+              >
+                <img
+                  style={{ width: 20 }}
+                  src="/assets/images/google.png"
+                  alt="Google"
+                />{" "}
+                Đăng Nhập Với Google
+              </Button>
+              <Button
+                variant="contained"
+                color="success"
+                fullWidth
+                sx={{ marginTop: 2 }}
+                onClick={() => navigate("/signup")}
+              >
+                Đăng ký
+              </Button>
+            </form>
+          </Box>
+        </Container>
+      </Box>
+      {modalOpen && (
+        <MessageModal
+          message={failMessage}
+          open={modalOpen}
+          handleClose={handleClose}
+          messageType={messageType}
         />
-        <button type="button" value={"Connect"} onClick={handleLogin}>
-          <span>Connect</span>
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
