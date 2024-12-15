1b7a6f170fb6148947f785284b33b64892c50120
quocthai <lxlthailxl@gmail.com>
2024-12-15 11:12:30 +0700

    add mapper user to user res

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/mapper/UserMapper.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package com.chatroomserver.chatroonbackend.mapper;
 
 import com.chatroomserver.chatroonbackend.dto.request.SignupRequest;
+import com.chatroomserver.chatroonbackend.dto.response.UserResponse;
 import com.chatroomserver.chatroonbackend.model.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(SignupRequest request);
+
+    UserResponse toUserResponse(User user);
 }
