dc4340ed2c0896c03e8eed6479d023c60f0449b7
quocthai <lxlthailxl@gmail.com>
2024-12-19 10:32:37 +0700

    update tailwind css conf

--- chatwithme-frontend/tailwind.config.js
+++ chatwithme-frontend/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [],
-};
+}
