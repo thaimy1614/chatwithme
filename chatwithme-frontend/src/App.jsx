48691ea423e19e911e958fb8ea813d270dda6d46
quocthai <lxlthailxl@gmail.com>
2024-12-19 09:54:34 +0700

    add profile and main chat page routes

--- chatwithme-frontend/src/App.jsx
+++ chatwithme-frontend/src/App.jsx
@@ -4,17 +4,51 @@ import { Login } from "./Layout/Login";
 import { ChatPage2 } from "./Layout/ChatPage2";
 import { Signup } from "./Layout/Signup";
 import Authenticate from "./component/common/Authenticate";
-axios.defaults.baseURL = import.meta.env.VITE_API_PREFIX || "http://localhost:8080";
+import ChatLayout from "./component/layouts/ChatLayout";
+import WithPrivateRoute from "./utils/WithPrivateRoute";
+import Profile from "./component/accounts/Profile";
+import { connectWebsocket } from "./services/ChatService";
+import { useEffect } from "react";
+import Header from "./component/layouts/Header";
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_PREFIX || "http://localhost:8080";
 axios.defaults.withCredentials = true;
 function App() {
+  useEffect(() => {
+    connectWebsocket();
+  }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/chat" element={<ChatPage2 />} />
-      <Route path="/authenticate" element={<Authenticate />} />
-    </Routes>
+    <>
+      <>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <WithPrivateRoute>
+                <ChatLayout />
+              </WithPrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <WithPrivateRoute>
+                <Profile />
+              </WithPrivateRoute>
+            }
+          />
+        </Routes>
+      </>
+      <>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/chat" element={<ChatPage2 />} />
+          <Route path="/authenticate" element={<Authenticate />} />
+        </Routes>
+      </>
+    </>
   );
 }
 
