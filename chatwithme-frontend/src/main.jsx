e881e553d3d933b2ac7c0b56215711a1cc856dfe
quocthai <lxlthailxl@gmail.com>
2024-12-19 09:54:59 +0700

    add footer 2 gifs

--- chatwithme-frontend/src/main.jsx
+++ chatwithme-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./component/common/ScrollToTop.jsx";
 
@@ -8,5 +9,19 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ScrollToTop />
     <App />
+    <div className="position-relative h-100">
+      <img
+        src="/assets/images/cute-dog.gif"
+        alt="Left Animal"
+        className="w-15 h-20 position-fixed bottom-0 start-0 mb-2 ms-2"
+        width="100"
+      />
+      <img
+        src="/assets/images/cute-panda.gif"
+        alt="Right Animal"
+        className="w-20 h-20 position-fixed bottom-0 end-0 mb-2 me-2"
+        width="100"
+      />
+    </div>
   </BrowserRouter>
 );
