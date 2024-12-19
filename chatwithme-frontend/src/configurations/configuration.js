6d68a4e65f82a85156e8e2fd435066b4ccc592e5
quocthai <lxlthailxl@gmail.com>
2024-12-19 09:57:00 +0700

    add oauth2 config

--- chatwithme-frontend/src/configurations/configuration.js
+++ chatwithme-frontend/src/configurations/configuration.js
@@ -1,7 +1,7 @@
 export const OAuthConfig = {
   clientId:
     "49452746846-m0mc09qdo3du2dnhbkg1ucabn7fq9lbl.apps.googleusercontent.com",
-  redirectUri: import.meta.env.REACT_APP_FRONTEND+"/authenticate",
+  redirectUri: import.meta.env.VITE_FRONTEND_PREFIX+"/authenticate",
   authUri: "https://accounts.google.com/o/oauth2/auth",
 };
 
