da9a85f5c297be19d002f0cf7c8d98aaf19ce7a0
quocthai <lxlthailxl@gmail.com>
2024-12-27 11:57:39 +0700

    add momo dto

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/momo/MomoCreationRequest.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/momo/MomoCreationRequest.java
@@ -1,2 +1,20 @@
-package com.chatroomserver.chatroonbackend.dto.momo;public class MomoCreationRequest {
+package com.chatroomserver.chatroonbackend.dto.momo;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MomoCreationRequest {
+    private String partnerCode;
+    private String requestType;
+    private String ipnUrl;
+    private String redirectUrl;
+    private String orderId;
+    private String amount;
+    private String orderInfo;
+    private String requestId;
+    private String extraData;
+    private String signature;
+    private String lang;
 }
--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/momo/MomoCreationResponse.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/dto/momo/MomoCreationResponse.java
@@ -1,2 +1,15 @@
-package com.chatroomserver.chatroonbackend.dto.momo;public class MomoCreationResponse {
+package com.chatroomserver.chatroonbackend.dto.momo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MomoCreationResponse {
+    private String resultCode;
+    private String payUrl;
+    private String deepLink;
+    private String qrCodeUrl;
 }
