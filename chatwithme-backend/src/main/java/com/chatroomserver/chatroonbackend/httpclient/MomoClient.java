2622fd542a2658d2d22f666b5ea2a3e60dd540bf
quocthai <lxlthailxl@gmail.com>
2024-12-27 11:57:09 +0700

    use feign client to call momo api to create payment url

--- chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/httpclient/MomoClient.java
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/httpclient/MomoClient.java
@@ -1,2 +1,13 @@
-package com.chatroomserver.chatroonbackend.httpclient;public interface MomoClient {
+package com.chatroomserver.chatroonbackend.httpclient;
+
+import com.chatroomserver.chatroonbackend.dto.momo.MomoCreationRequest;
+import com.chatroomserver.chatroonbackend.dto.momo.MomoCreationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "momo-client", url = "${momo.base-url}")
+public interface MomoClient {
+    @PostMapping(value = "/v2/gateway/api/create")
+    MomoCreationResponse createUrl(@RequestBody MomoCreationRequest request);
 }
