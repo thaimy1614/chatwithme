760006d4d0a895aa83511ef84be99f7e26071ea8
quocthai <lxlthailxl@gmail.com>
2024-12-27 11:54:11 +0700

    add momo utils (encode hmacSHA256 and sort params a-z by using map)

---
+++ chatwithme-backend/src/main/java/com/chatroomserver/chatroonbackend/util/MomoUtil.java
@@ -0,0 +1,44 @@
+package com.chatroomserver.chatroonbackend.util;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MomoUtil {
+
+    public static String getPaymentURL(Map<String, String> paramsMap, boolean encodeKey) {
+        return paramsMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry ->
+                        (encodeKey ? URLEncoder.encode(entry.getKey(),
+                                StandardCharsets.US_ASCII)
+                                : entry.getKey()) + "=" + entry.getValue()
+                )
+                .collect(Collectors.joining("&"));
+    }
+
+    @SuppressWarnings("resource")
+    private static String toHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+        Formatter formatter = new Formatter(sb);
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        return sb.toString();
+    }
+
+    public static String encode(String secretKey, String data) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(secretKeySpec);
+        byte[] rawHmac = mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return toHexString(rawHmac);
+    }
+}
