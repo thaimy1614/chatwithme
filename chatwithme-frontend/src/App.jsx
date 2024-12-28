e08e9bdab3a38da358a0849e32903cfea150bd84
quocthai <lxlthailxl@gmail.com>
2024-12-28 22:39:56 +0700

    update app

--- chatwithme-frontend/src/App.jsx
+++ chatwithme-frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Profile from "./component/accounts/Profile";
 import { connectWebsocket } from "./services/ChatService";
 import { useEffect } from "react";
 import Header from "./component/layouts/Header";
+import { UserProvider } from "./context/UserContext";
 axios.defaults.baseURL =
   import.meta.env.VITE_API_PREFIX || "http://localhost:8080";
 axios.defaults.withCredentials = true;
@@ -17,38 +18,35 @@ function App() {
   useEffect(() => {
     connectWebsocket();
   }, []);
+
   return (
-    <>
-      <>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <WithPrivateRoute>
-                <ChatLayout />
-              </WithPrivateRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <WithPrivateRoute>
-                <Profile />
-              </WithPrivateRoute>
-            }
-          />
-        </Routes>
-      </>
-      <>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/chat" element={<ChatPage2 />} />
-          <Route path="/authenticate" element={<Authenticate />} />
-        </Routes>
-      </>
-    </>
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <WithPrivateRoute>
+              <ChatLayout />
+            </WithPrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <WithPrivateRoute>
+              <Profile />
+            </WithPrivateRoute>
+          }
+        />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<ChatPage2 />} />
+        <Route path="/authenticate" element={<Authenticate />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
